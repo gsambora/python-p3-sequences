dfc3f9787f5a813cd1ed289ad7e753dc981e940d
Hana Sambora <grace.sambora@gmail.com>
2023-11-30 10:06:17 -0600

    Complete lab

--- lib/sequences.py
+++ lib/sequences.py
@@ -1,4 +1,21 @@
 #!/usr/bin/env python3
 
 def print_fibonacci(length):
-    pass
+    fibList = []
+    if length > 0:
+        for lenSpot in range(length):
+            if lenSpot == 0 :
+                fibList.append(0)
+            elif lenSpot == 1:
+                fibList.append(1)
+            else:
+                # print ("current list ", fibList)
+                # print("lenSpot ", lenSpot)
+                # print("fibList[1] = ", fibList[lenSpot-1])
+                if len(fibList) >= 2:
+                    nextValue = fibList[lenSpot-1] + fibList[lenSpot-2]
+                    fibList.append(nextValue)
+    
+    print(fibList)
+
+print_fibonacci(3)
